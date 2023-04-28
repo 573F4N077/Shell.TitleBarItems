cc0592835fe43d7298c0499d206f4e1ec71354be
Stefan Ott <stefan.ott@outlook.com>
2023-04-28 09:27:17 +0200

    Initial Upload

---
+++ Test Project Template/Test Project Template/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Test_Project_Template;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Test Project Template/Test Project Template/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Test_Project_Template;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
